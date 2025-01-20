bc1589cf4ec18207020fc20811677278b75588af
JavonteRobins <JavonteR10@yahoo.com>
2025-01-20 10:07:00 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_Pgm00CxYE', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6pp6ji1v2g7ne4go2gpie9097v', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_BHuncGnd9', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '332ctt34g3o1v0f046mj6rnhgj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
